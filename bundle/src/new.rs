6a3fda60e56c3d3258e8a76d312456360bdf258f
TheTacBanana <github@viviangilmore.com>
2024-08-25 22:46:00 +0100

    Create new project with bundle

--- bundle/src/new.rs
+++ bundle/src/new.rs
@@ -1,26 +1,59 @@
-use std::path::{Path, PathBuf};
+use std::{
+    io::Write,
+    path::{Path, PathBuf},
+};
 
-use clap::Args;
 use anyhow::{anyhow, Context, Error, Ok, Result};
+use clap::Args;
 
-use crate::cli::CommandTrait;
-
+use crate::{cli::CommandTrait, main};
 
 #[derive(Debug, Args)]
 pub struct New {
     project_name: String,
 }
 
-impl CommandTrait for New {
-    fn execute(&mut self) -> Result<()>{
-        let mut path = PathBuf::new();
-        path.push(&self.project_name);
+impl New {
+    const DEFAULT_BUNDLE_TOML: &'static str = include_str!("Bundle.toml");
+    const DEFAULT_MAIN_ECSL: &'static str = include_str!("main.ecsl");
+}
 
-        if path.exists() {
+impl CommandTrait for New {
+    fn execute(&mut self) -> Result<()> {
+        let mut root_path = PathBuf::new();
+        root_path.push(&self.project_name);
+
+        if root_path.exists() {
             return Err(anyhow!("Project {:?} already exists", self.project_name));
         }
 
-        Ok(())
+        // Create the project directory
+        std::fs::create_dir(root_path.clone())?;
 
+        // Create the Bundle.toml
+        {
+            let mut bundle_toml = root_path.clone();
+            bundle_toml.push("Bundle.toml");
+            let mut bundle_toml = std::fs::File::create_new(bundle_toml)?;
+            let contents = New::DEFAULT_BUNDLE_TOML.replace("{}", &self.project_name);
+            bundle_toml.write_all(contents.as_bytes())?;
+        }
+
+        // Create the src directory
+        {
+            let mut src_dir = root_path.clone();
+            src_dir.push("src");
+            std::fs::create_dir(src_dir)?
+        }
+
+        // Create the main file
+        {
+            let mut main_ecsl = root_path.clone();
+            main_ecsl.push("src/main.ecsl");
+            let mut main_ecsl = std::fs::File::create_new(main_ecsl)?;
+            main_ecsl.write_all(New::DEFAULT_MAIN_ECSL.as_bytes())?
+        }
+
+        Ok(())
     }
 }
