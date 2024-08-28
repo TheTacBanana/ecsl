3973e30068e96ec8b4f6aa6a25ecad7bea6cc5c5
TheTacBanana <github@viviangilmore.com>
2024-08-28 16:03:44 +0100

    Token Kinds

--- ecsl_lexer/src/token.rs
+++ ecsl_lexer/src/token.rs
@@ -4,8 +4,89 @@ pub struct Token {
     kind: TokenKind,
 }
 
+impl Token {
+    pub fn new(kind: TokenKind, length: u32) -> Self {
+        Self { length, kind }
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum TokenKind {
-    Identifier,
+    /// "// This is a comment"
+    Comment,
+
+    /// Any sequence of whitespace characters
     Whitespace,
+
+    /// Any valid identifer
+    Identifier,
+
+    /// Literals
+    Literal(LiteralKind),
+
+    // Single Character Tokens
+    /// ";"
+    Semi,
+    /// ","
+    Comma,
+    /// "."
+    Dot,
+    /// "("
+    OpenParen,
+    /// ")"
+    CloseParen,
+    /// "{"
+    OpenBrace,
+    /// "}"
+    CloseBrace,
+    /// "["
+    OpenBracket,
+    /// "]"
+    CloseBracket,
+    /// "#"
+    Hash,
+    /// ":"
+    Colon,
+    /// "="
+    Eq,
+    /// "!"
+    Bang,
+    /// "<"
+    Lt,
+    /// ">"
+    Gt,
+    /// "+"
+    Plus,
+    /// "-"
+    Minus,
+    /// "*"
+    Star,
+    /// "/"
+    Slash,
+    /// "&"
+    Ampersand,
+    /// "|"
+    Pipe,
+
+    /// End of File
+    Eof,
+
+    /// Unknown character, most likely unicode
+    Unknown,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum LiteralKind {
+    Int { base: Base },
+    Float,
+    String { terminated: bool },
+    Char { terminated: bool },
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Base {
+    Binary = 2,
+    Octal = 8,
+    Decimal = 10,
+    Hexadecimal = 16,
 }
