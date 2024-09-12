4ca20f64fd89a5f7e4c216861d5cc614817bfe5a
TheTacBanana <github@viviangilmore.com>
2024-09-12 16:58:41 +0100

    Struct, Comp, Enum Definitions

---
+++ ecsl_ast/src/data.rs
@@ -0,0 +1,40 @@
+use ecsl_span::Span;
+
+use crate::{item::Item, ty::{Generics, Ty}, Ident, P};
+
+#[derive(Debug, Clone)]
+pub struct StructDef {
+    span: Span,
+    kind: StructKind,
+    ident: Ident,
+    generics: P<Generics>,
+    fields: Vec<P<Field>>,
+}
+
+#[derive(Debug, Clone)]
+pub enum StructKind {
+    Struct,
+    Comp,
+}
+
+#[derive(Debug, Clone)]
+pub struct EnumDef {
+    span: Span,
+    ident: Ident,
+    generics: P<Generics>,
+    fields: Vec<P<Variant>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Field {
+    span: Span,
+    ident: Ident,
+    ty: P<Ty>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Variant {
+    span: Span,
+    ident: Ident,
+    fields: Vec<P<Field>>,
+}
