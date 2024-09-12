dcc1b56bf30e1fc4eee0119ce1fb92c8c5c7ae26
TheTacBanana <github@viviangilmore.com>
2024-09-12 16:58:27 +0100

    Fn Definitions

---
+++ ecsl_ast/src/callable.rs
@@ -0,0 +1,32 @@
+use ecsl_span::Span;
+
+use crate::{stmt::Block, ty::Ty, Ident, P};
+
+#[derive(Debug, Clone)]
+pub struct FnDef {
+    span: Span,
+    kind: FnKind,
+    ident: Ident,
+    params: Vec<Param>,
+    ret: RetTy,
+    block: Block,
+}
+
+#[derive(Debug, Clone)]
+pub struct Param {
+    span: Span,
+    ident: Ident,
+    ty: P<Ty>
+}
+
+#[derive(Debug, Clone)]
+pub enum FnKind {
+    Fn,
+    Sys,
+}
+
+#[derive(Debug, Clone)]
+pub enum RetTy {
+    None(Span),
+    Ty(P<Ty>),
+}
