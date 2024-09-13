ef5bd4a34d1ae016d584f590f2eb0a630f434276
TheTacBanana <github@viviangilmore.com>
2024-09-13 11:09:13 +0100

    Type Test case definitions

--- ecsl_ast/src/lib.rs
+++ ecsl_ast/src/lib.rs
@@ -10,3 +10,72 @@ pub mod ecs;
 pub (crate) type P<T> = Box<T>;
 
 pub (crate) type Ident = ();
+
+#[cfg(test)]
+mod test {
+    /// Test cases for Type Parsing
+    mod ty {
+        #[test]
+        fn ident() {
+            "Foo";
+            "Foo<Bar>";
+            "Foo<Bar, Baz>";
+            "Foo<Bar<Baz>, Qux>";
+        }
+
+        #[test]
+        fn array() {
+            "[Foo : 5]";
+            "[Foo<Bar> : 5]";
+        }
+
+        #[test]
+        fn array_ref() {
+            "&[Foo]";
+            "&[Foo<Bar>]";
+        }
+
+        #[test]
+        fn ref_() {
+            "&Foo";
+            "&Foo<Bar>";
+            "&mut Bar";
+            "&mut Bar<Baz>";
+        }
+
+        #[test]
+        fn ptr() {
+            "*imm Foo";
+            "*imm Foo<Bar>";
+            "*mut Bar";
+            "*mut Baz<Qux>";
+        }
+
+        #[test]
+        fn entity() {
+            "Entity";
+            "Entity<foo: Foo>";
+            "Entity<foo: mut Foo>";
+            "Entity<foo: Bar, baz: mut Qux>";
+            "Entity<foo: Bar, ..>";
+        }
+
+        #[test]
+        fn query() {
+            "Query";
+            "Query<With<Foo>>";
+            "Query<Without<Bar>>";
+            "Query<With<Baz>,Without<Qux>>";
+        }
+
+        #[test]
+        fn system() {
+            "System";
+        }
+
+        #[test]
+        fn schedule() {
+            "Schedule";
+        }
+    }
+}
