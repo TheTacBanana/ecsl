e28003a47735a3bb42a8316a0f62e016260c4a51
TheTacBanana <github@vivianeris.com>
2024-11-29 13:57:10 +0000

    Local validation stuff

--- ecsl_ast_validation/src/lib.rs
+++ ecsl_ast_validation/src/lib.rs
@@ -1,24 +1,31 @@
+use std::sync::Arc;
+
+use definitions::TypeDefCollector;
 use ecsl_ast::{visit::Visitor, SourceAST};
 use ecsl_error::EcslError;
-use ecsl_parse::table::SymbolTable;
+use ecsl_ty::LocalTyCtxt;
 use fnvalidator::FnValidator;
 use imports::ImportCollector;
 
-pub mod exports;
+pub mod definitions;
 pub mod fnvalidator;
 pub mod imports;
+pub mod locals;
 
-pub fn validate_ast(ast: &SourceAST, table: &SymbolTable) -> Vec<EcslError> {
+pub fn validate_ast(ast: &SourceAST, ty_ctxt: Arc<LocalTyCtxt>) -> Vec<EcslError> {
     let mut errors = Vec::new();
 
     let mut fn_validator = FnValidator::new();
     fn_validator.visit_ast(ast);
 
-    let mut imports = ImportCollector::new(table);
+    let mut definitions = TypeDefCollector::new(ty_ctxt.clone());
+    definitions.visit_ast(ast);
+
+    let mut imports = ImportCollector::new(ty_ctxt.clone());
     imports.visit_ast(ast);
 
     errors.extend(fn_validator.errors);
-    errors.extend(imports.errors);
+    errors.extend(ty_ctxt.errors.write().unwrap().drain(..));
 
     errors
 }
