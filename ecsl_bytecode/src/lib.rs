d1dfc117c5c8808b4ca3ea55d0b1a331b2d3c179
TheTacBanana <github@vivianeris.com>
2024-12-10 01:10:00 +0000

    Generate opcode.zig

--- ecsl_bytecode/src/lib.rs
+++ ecsl_bytecode/src/lib.rs
@@ -17,107 +17,107 @@ pub enum ImmediateLong {
 
 #[derive(Debug, Bytecode)]
 pub enum Bytecode {
-    // No Op
+    /// No Op
     NOP,
-    // Halt the program
+    /// Halt the program
     HALT,
 
-    // Pop the top byte of the stack
+    /// Pop the top byte of the stack
     POPB,
-    // Pop the top (4 bytes) of the stack
+    /// Pop the top (4 bytes) of the stack
     POP,
-    // Pop the top (8 bytes) of the stack
+    /// Pop the top (8 bytes) of the stack
     POPL,
-    // Duplicate the top value
+    /// Duplicate the top value
     DUP,
-    // Push the immediate value
+    /// Push the immediate value
     PSHI(Immediate),
-    // Push the long immediate value
+    /// Push the long immediate value
     PSHIL(ImmediateLong),
-    // Load N bytes from the address onto the stack
+    /// Load N bytes from the address onto the stack
     LD(Address, Immediate),
-    // Store the top N bytes from the stack at the address
+    /// Store the top N bytes from the stack at the address
     ST(Address, Immediate),
 
-    // Compare 2 Bytes and push the result (Integer)
+    /// Compare 2 Bytes and push the result (Integer)
     CMPB,
-    // Compare 2 Integers and push the result (Integer)
+    /// Compare 2 Integers and push the result (Integer)
     CMPI,
-    // Compare 2 Longs and push the result (Integer)
+    /// Compare 2 Longs and push the result (Integer)
     CMPL,
-    // Unconditional Jump
+    /// Unconditional Jump
     JMP(Address),
-    // Jump Greater than or Equal to
+    /// Jump Greater than or Equal to
     JGE(Address),
-    // Jump Greater Than
+    /// Jump Greater Than
     JGT(Address),
-    // Jump Less than or Equal to
+    /// Jump Less than or Equal to
     JLE(Address),
-    // Jump Less than
+    /// Jump Less than
     JLT(Address),
-    // Jump Equal to 0
+    /// Jump Equal to 0
     JEZ(Address),
-    // Jump Not 0
+    /// Jump Not 0
     JNZ(Address),
 
-    // Add the top 2 bytes of the stack and push the result
+    /// Add the top 2 bytes of the stack and push the result
     ADDB,
-    // Sub the top 2 bytes of the stack and push the result
+    /// Sub the top 2 bytes of the stack and push the result
     SUBB,
-    // Mul the top 2 bytes of the stack and push the result
+    /// Mul the top 2 bytes of the stack and push the result
     MULB,
-    // Div the top 2 bytes of the stack and push the result
+    /// Div the top 2 bytes of the stack and push the result
     DIVB,
 
-    // Add the top 2 integers of the stack and push the result
+    /// Add the top 2 integers of the stack and push the result
     ADDI,
-    // Sub the top 2 integers of the stack and push the result
+    /// Sub the top 2 integers of the stack and push the result
     SUBI,
-    // Mul the top 2 integers of the stack and push the result
+    /// Mul the top 2 integers of the stack and push the result
     MULI,
-    // Div the top 2 integers of the stack and push the result
+    /// Div the top 2 integers of the stack and push the result
     DIVI,
 
-    // Add the top 2 longs of the stack and push the result
+    /// Add the top 2 longs of the stack and push the result
     ADDL,
-    // Sub the top 2 longs of the stack and push the result
+    /// Sub the top 2 longs of the stack and push the result
     SUBL,
-    // Mul the top 2 longs of the stack and push the result
+    /// Mul the top 2 longs of the stack and push the result
     MULL,
-    // Div the top 2 longs of the stack and push the result
+    /// Div the top 2 longs of the stack and push the result
     DIVL,
 
-    // Add the top 2 floats of the stack and push the result
+    /// Add the top 2 floats of the stack and push the result
     ADDF,
-    // Sub the top 2 floats of the stack and push the result
+    /// Sub the top 2 floats of the stack and push the result
     SUBF,
-    // Mul the top 2 floats of the stack and push the result
+    /// Mul the top 2 floats of the stack and push the result
     MULF,
-    // Div the top 2 floats of the stack and push the result
+    /// Div the top 2 floats of the stack and push the result
     DIVF,
 
-    // Add the top 2 addresses of the stack and push the result
+    /// Add the top 2 addresses of the stack and push the result
     ADDA,
-    // Sub the top 2 addresses of the stack and push the result
+    /// Sub the top 2 addresses of the stack and push the result
     SUBA,
-    // Mul the top 2 addresses of the stack and push the result
+    /// Mul the top 2 addresses of the stack and push the result
     MULA,
 
-    // Cast integer to long
+    /// Cast integer to long
     ITL,
-    // Cast long to integer
+    /// Cast long to integer
     LTI,
 
-    // Shift Left (Byte)
+    /// Shift Left (Byte)
     SHL,
-    // Shift Right (Byte)
+    /// Shift Right (Byte)
     SHR,
-    // Bitwise Negation (Byte)
+    /// Bitwise Negation (Byte)
     NEG,
-    // Bitwise AND (Byte)
+    /// Bitwise AND (Byte)
     AND,
-    // Bitwise OR (Byte)
+    /// Bitwise OR (Byte)
     OR,
-    // Bitwise XOR (Byte)
+    /// Bitwise XOR (Byte)
     XOR
 }
--- ecsl_bytecode_derive/src/lib.rs
+++ ecsl_bytecode_derive/src/lib.rs
@@ -5,7 +5,8 @@ extern crate quote;
 
 use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DataEnum, DeriveInput, Fields, PathSegment};
+use std::io::Write;
+use syn::{parse_macro_input, Data, DataEnum, DeriveInput, Expr, Lit};
 
 const PATH_TO_VM_SRC: &str = "./ecsl_vm/src/";
 
@@ -13,15 +14,18 @@ const PATH_TO_VM_SRC: &str = "./ecsl_vm/src/";
 pub fn ast_attribute(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
 
-
-    let Data::Enum(e) = ast.data else { panic!("Only enums supported")};
+    let Data::Enum(e) = ast.data else {
+        panic!("Only enums supported")
+    };
 
     let out = generate_opcode_enum(&e);
-    // panic!("{out}");
+
+    zig_opcode_generation(&e);
+
     out.into()
 }
 
-fn generate_opcode_enum(e: &DataEnum) -> TokenStream{
+fn generate_opcode_enum(e: &DataEnum) -> TokenStream {
     let mut variant_names = Vec::new();
     let mut variant_match_arms = Vec::new();
 
@@ -31,11 +35,9 @@ fn generate_opcode_enum(e: &DataEnum) -> TokenStream{
 
         let ident = variant.ident.clone();
         let operand_len = variant.fields.len();
-        variant_match_arms.push(
-            quote! {
-                #ident => #operand_len
-            }
-        );
+        variant_match_arms.push(quote! {
+            #ident => #operand_len
+        });
     }
 
     quote! {
@@ -54,6 +56,47 @@ fn generate_opcode_enum(e: &DataEnum) -> TokenStream{
     }
 }
 
-// fn zig_opcode_generation(e: &DataEnum) {
-//     std::fs::File::create(format!("{PATH_TO_VM_SRC}/temp_file")).unwrap();
-// }
+fn zig_opcode_generation(e: &DataEnum) {
+    let mut variant_names = String::new();
+
+    for variant in e.variants.iter() {
+        let mut docs = String::new();
+        for c in &variant.attrs {
+            match &c.meta {
+                syn::Meta::NameValue(pair) if pair.path.segments[0].ident == "doc" => {
+                    let Expr::Lit(l) = &pair.value else {
+                        continue;
+                    };
+                    let Lit::Str(token) = &l.lit else {
+                        continue;
+                    };
+
+                    docs.push_str(&format!("///{}\n\t", token.value()));
+                }
+                _ => (),
+            }
+        }
+        variant_names.push_str(&format!("{}{},\n\t", docs, variant.ident.to_string()));
+    }
+
+    let operations_zig = format!(
+        r#"const std = @import("std");
+const ins = @import("instruction.zig");
+
+pub const Opcode = enum(u8) {{
+    {}
+
+    pub fn from_byte(b: u8) error{{InvalidInstruction}}!Opcode {{
+        return std.meta.intToEnum(Opcode, b) catch return error.InvalidInstruction;
+    }}
+}};
+
+"#,
+        variant_names.trim_end()
+    );
+
+    std::fs::File::create(format!("{PATH_TO_VM_SRC}/opcode.zig"))
+        .unwrap()
+        .write_all(operations_zig.as_bytes())
+        .unwrap();
+}
